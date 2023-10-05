f4cfdf70d4c3c804393e5d491acd229587784085
Anastasia <90128050+SantAlice@users.noreply.github.com>
2023-10-06 02:06:44 +0300

    Reformate code

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -20,7 +20,7 @@ public class CardDeliveryTest {
     public void successCompleting() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Екатеринбург");
-        String planningDate = generateDate(4,"dd.MM.yyyy");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").doubleClick().sendKeys(Keys.DELETE);
         $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иван Кашкаров");
